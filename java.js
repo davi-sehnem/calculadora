14fda0426e04eceea9bebbdd3c659b7f5f2caad9
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-18 09:16:05 -0300

    feat - javascript

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
-function add() {
-    
+let display = document.getElementById("display");
+let currentInput = "";
+
+
+function add(input){
+    currentInput += input;
+    display.textContent = currentInput;
+}
+
+function limpar(){
+
+}
+
+function calcula(){
+
 }
